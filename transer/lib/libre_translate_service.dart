af5a1058dd69be7e0a9ddcf2550ecd0330f8ce3b
Udaiyaa-05 <udaiyaa.s2005@gmail.com>
2025-07-18 19:22:45 +0530

    Created libre_translate

--- transer/lib/libre_translate_service.dart
+++ transer/lib/libre_translate_service.dart
@@ -1,1 +1,30 @@
-// TODO Implement this library.
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+Future<String> translateWithLibre(String text, String fromLang, String toLang) async {
+  final url = Uri.parse("http://localhost:5000/translate");
+  try {
+    final response = await http.post(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: jsonEncode({
+        'q': text,
+        'source': fromLang,
+        'target': toLang,
+        'format': 'text',
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      return data['translatedText'];
+    } else {
+      return "LibreTranslate error: ${response.statusCode}";
+    }
+  } catch (e) {
+    return "Translation failed: $e";
+  }
+}
